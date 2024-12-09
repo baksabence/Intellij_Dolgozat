2e275bcad5e22669720ff79f06b9ff4151a5d875
baksabence <gxdcloud@gmail.com>
2024-12-09 14:33:18 +0100

    f1 5

--- src/main/java/hu/szamalk/Main.java
+++ src/main/java/hu/szamalk/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Tabla tabla = new Tabla('#');
         tabla.Megjelenit();
+
     }
 }
--- src/main/java/hu/szamalk/Tabla.java
+++ src/main/java/hu/szamalk/Tabla.java
@@ -1,8 +1,12 @@
 package hu.szamalk;
 
+import java.util.Random;
+
 public class Tabla {
     private char[][] T;
     private char ÜresCella;
+
+    private static final Random rnd = new Random();
 
     public Tabla(char üresCella) {
         T = new char[8][8];
@@ -20,6 +24,7 @@ public class Tabla {
                 System.out.print(T[i][j]);
             }
             System.out.println();
+            Elhelyez(8);
         }
 //        for (int i = 0; i < T.length; i++) {
 //                for (int j = 0; j < T.length; j++) {
@@ -27,5 +32,18 @@ public class Tabla {
 //                }
 //        }
     }
+
+    public void Elhelyez(int n){
+        int db = n;
+        int veletlenHelyre = rnd.nextInt(0, 8);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(T[veletlenHelyre][veletlenHelyre] == '#')
+                    T[veletlenHelyre][veletlenHelyre] = 'K';
+            }
+            }
+
+    }
+
 
 }
