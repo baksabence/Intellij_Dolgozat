f2321c5ef24aa5c2a9673612a5f2ba063f646f9c
baksabence <gxdcloud@gmail.com>
2024-12-09 13:38:08 +0100

    f1 3

--- src/main/java/hu/szamalk/Tabla.java
+++ src/main/java/hu/szamalk/Tabla.java
@@ -4,5 +4,14 @@ public class Tabla {
     private char[][] T;
     private char ÜresCella;
 
-    
+    public Tabla(char üresCella) {
+        T = new char[8][8];
+        ÜresCella = üresCella;
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T.length; j++) {
+                T[i][j] = ÜresCella;
+            }
+        }
+    }
+
 }
